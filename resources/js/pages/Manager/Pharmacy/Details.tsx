71df3dc0063422f6f40f54af49f1e06d52921e8f
viwemhlaba <viwemhlabavm@gmail.com>
2025-05-19 00:35:44 +0200

    Fix lint and test issues

--- resources/js/pages/Manager/Pharmacy/Details.tsx
+++ resources/js/pages/Manager/Pharmacy/Details.tsx
@@ -3,6 +3,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
+
 import AppLayout from '@/layouts/app-layout';
 import { cn } from '@/lib/utils';
 import { type BreadcrumbItem } from '@/types';
