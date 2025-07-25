99de75e54aa83d76f9d7ad5aec2e171a4aa07a40
viwemhlaba <viwemhlabavm@gmail.com>
2025-07-25 18:43:43 +0200

    Fix migration: Remove Faker dependency for production compatibility

--- database/migrations/2025_07_06_200000_update_existing_customers_with_missing_details.php
+++ database/migrations/2025_07_06_200000_update_existing_customers_with_missing_details.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Customer;
-use Faker\Factory as Faker;
 
 return new class extends Migration
 {
@@ -14,22 +13,25 @@ return new class extends Migration
     public function up(): void
     {
         // Update existing customers that don't have proper details
-        $faker = Faker::create();
         $customersWithoutIdNumber = Customer::whereNull('id_number')->orWhere('id_number', '')->get();
         
-        foreach ($customersWithoutIdNumber as $customer) {
+        $states = [
+            'Eastern Cape', 'Free State', 'Gauteng', 'KwaZulu-Natal',
+            'Limpopo', 'Mpumalanga', 'Northern Cape', 'North West', 'Western Cape'
+        ];
+        
+        $allergies = ['None', 'Peanuts', 'Dust', 'Pollen'];
+        
+        foreach ($customersWithoutIdNumber as $index => $customer) {
             $customer->update([
-                'id_number' => $faker->unique()->numerify('##########'),
-                'cellphone_number' => $customer->cellphone_number ?: $faker->phoneNumber(),
-                'allergies' => $customer->allergies ?: $faker->randomElement(['None', 'Peanuts', 'Dust', 'Pollen']),
-                'state' => $customer->state ?: $faker->randomElement([
-                    'Eastern Cape', 'Free State', 'Gauteng', 'KwaZulu-Natal',
-                    'Limpopo', 'Mpumalanga', 'Northern Cape', 'North West', 'Western Cape',
-                ]),
-                'city' => $customer->city ?: $faker->city(),
-                'street' => $customer->street ?: $faker->streetName(),
-                'house_number' => $customer->house_number ?: $faker->buildingNumber(),
-                'postal_code' => $customer->postal_code ?: $faker->postcode(),
+                'id_number' => $customer->id_number ?: str_pad((string)(8000000000 + $customer->id + $index), 10, '0', STR_PAD_LEFT),
+                'cellphone_number' => $customer->cellphone_number ?: '+27' . str_pad((string)(600000000 + $customer->id), 9, '0', STR_PAD_LEFT),
+                'allergies' => $customer->allergies ?: $allergies[($customer->id % count($allergies))],
+                'state' => $customer->state ?: $states[($customer->id % count($states))],
+                'city' => $customer->city ?: 'City ' . $customer->id,
+                'street' => $customer->street ?: 'Street ' . $customer->id,
+                'house_number' => $customer->house_number ?: (string)($customer->id % 999 + 1),
+                'postal_code' => $customer->postal_code ?: str_pad((string)(1000 + ($customer->id % 8999)), 4, '0', STR_PAD_LEFT),
             ]);
         }
     }
